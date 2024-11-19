e54f1c08b8a899bb5ad86b9572b26a53a995bb95
steven-felim <stevenfelim04@gmail.com>
2024-11-19 14:40:26 +0700

    add voucher class

---
+++ src/Voucher.java
@@ -0,0 +1,35 @@
+public class Voucher {
+    private String voucher;
+    private double discount;
+    private boolean used;
+
+    public Voucher(String voucher, double discount, boolean used) {
+        this.voucher = voucher;
+        this.discount = discount;
+        this.used = used;
+    }
+
+    public String getVoucher() {
+        return voucher;
+    }
+
+    public void setVoucher(String voucher) {
+        this.voucher = voucher;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+}
