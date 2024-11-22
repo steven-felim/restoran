1e5da787cf87926d3464384ab9f282793019265d
steven-felim <stevenfelim04@gmail.com>
2024-11-22 11:57:57 +0700

    Update point class

---
+++ src/model/classes/Point.java
@@ -0,0 +1,13 @@
+package model.classes;
+
+public class Point {
+    private int point;
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+}
