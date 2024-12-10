b50a3b304b0d68f283c50df6395c99dc654f5510
steven-felim <stevenfelim04@gmail.com>
2024-12-10 19:15:08 +0700

    update BookTable class

--- src/model/classes/BookTable.java
+++ src/model/classes/BookTable.java
@@ -1,16 +1,19 @@
-import java.util.Calendar;
+package model.classes;
+
+import model.enums.BookStatus;
+import java.util.Date;
 
 public class BookTable {
     private int bookID;
-    private Room room;
+    private Table table;
     private User user;
     private Date date;
     private BookStatus status;
 
     
-    public BookTable(int bookID, Room room, User user, Date date, BookStatus status) {
+    public BookTable(int bookID, Table table, User user, Date date, BookStatus status) {
         this.bookID = bookID;
-        this.room = room;
+        this.table = table;
         this.user = user;
         this.date = date;
         this.status = status;
@@ -24,12 +27,12 @@ public class BookTable {
         this.bookID = bookID;
     }
 
-    public Room getRoom() {
-        return room;
+    public Table getTable() {
+        return table;
     }
 
-    public void setRoom(Room room) {
-        this.room = room;
+    public void setTable(Table table) {
+        this.table = table;
     }
 
     public User getUser() {
