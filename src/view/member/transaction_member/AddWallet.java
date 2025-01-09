4aebac12c04588d91f58ed01f6ef014695297468
JoChAl51 <jcanassgamers04@gmail.com>
2025-01-09 13:54:15 +0700

    WalletController

--- src/view/member/transaction_member/AddWallet.java
+++ src/view/member/transaction_member/AddWallet.java
@@ -1,6 +1,9 @@
 package view.member.transaction_member;
 
 import controller.AuthenticationController;
+import controller.AuthenticationHelper;
+import controller.MemberController;
+import controller.WalletController;
 import view.member.MemberMenu;
 
 import java.awt.Color;
@@ -34,7 +37,9 @@ public class AddWallet extends JFrame {
         panel.setLayout(null);
 
         // Balance Label
-        balanceLabel = new JLabel("Current Balance: Rp"); // controller get wallet
+        balanceLabel = new JLabel(
+        "Current Balance: Rp" + new WalletController().getWalletMember().getBalance()
+        );
         balanceLabel.setBounds(10, 20, 250, 25);
         panel.add(balanceLabel);
         
@@ -61,6 +66,21 @@ public class AddWallet extends JFrame {
         JButton profileButton = new JButton("Back");
         profileButton.setBounds(200, 140, 150, 25);
         panel.add(profileButton);
+
+        addButton.addActionListener(e -> {
+            boolean result = new WalletController().addBalanceMember(
+                    amountField.getText(),
+                    new String(pinField.getPassword())
+            );
+            if (result) {
+                amountField.setText("");
+                pinField.setText("");
+                balanceLabel.setText(
+                    "Current Balance: Rp" +
+                    new WalletController().getWalletMember().getBalance()
+                );
+            }
+        });
 
         profileButton.addActionListener(e -> {
             this.dispose();
